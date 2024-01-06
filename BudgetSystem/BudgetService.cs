082b954ced2f718c901ad5e1c3635678ca17fc00
Joey Chen <jay.hatelove@gmail.com>
2024-01-06 19:21:04 +0800

    add check start, end and middle month condition

--- BudgetSystem/BudgetService.cs
+++ BudgetSystem/BudgetService.cs
@@ -51,48 +51,45 @@ public class BudgetService
                     var overlappingDays = end.Day - start.Day + 1;
                     return overlappingDays * (budget.Amount / days);
                 }
-
-                // dateRange = new List<DateResult>()
-                //             {
-                //                 new DateResult()
-                //                 {
-                //                     Year = start.Year,
-                //                     Month = start.Month,
-                //                     Days = overlappingDays
-                //                 }
-                //             };
             }
             else
             {
                 var startDays = DateTime.DaysInMonth(start.Year, start.Month);
 
-                dateRange.Add(new DateResult()
-                              {
-                                  Year = start.Year,
-                                  Month = start.Month,
-                                  Days = startDays - start.Day + 1
-                              });
-
-                dateRange.Add(new DateResult()
-                              {
-                                  Year = end.Year,
-                                  Month = end.Month,
-                                  Days = end.Day
-                              });
-
-                var secondMonth = start.AddMonths(1);
-
-                for (var date = secondMonth; date < end; date.AddMonths(1))
+                if (budget.YearMonth == start.ToString("yyyyMM"))
                 {
-                    if (date.Year == end.Year && date.Month == end.Month)
-                        break;
-
                     dateRange.Add(new DateResult()
                                   {
-                                      Year = date.Year,
-                                      Month = date.Month,
-                                      Days = DateTime.DaysInMonth(date.Year, date.Month)
+                                      Year = start.Year,
+                                      Month = start.Month,
+                                      Days = startDays - start.Day + 1
                                   });
+                }
+                else if (budget.YearMonth == end.ToString("yyyyMM"))
+                {
+                    dateRange.Add(new DateResult()
+                                  {
+                                      Year = end.Year,
+                                      Month = end.Month,
+                                      Days = end.Day
+                                  });
+                }
+                else
+                {
+                    var secondMonth = start.AddMonths(1);
+
+                    for (var date = secondMonth; date < end; date.AddMonths(1))
+                    {
+                        if (date.Year == end.Year && date.Month == end.Month)
+                            break;
+
+                        dateRange.Add(new DateResult()
+                                      {
+                                          Year = date.Year,
+                                          Month = date.Month,
+                                          Days = DateTime.DaysInMonth(date.Year, date.Month)
+                                      });
+                    }
                 }
             }
         }
