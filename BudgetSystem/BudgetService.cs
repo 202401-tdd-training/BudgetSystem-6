c6d3ee0d9f4a3d91ecda5244ff42c2e308c11ce9
Joey Chen <jay.hatelove@gmail.com>
2024-01-06 20:06:32 +0800

    clean up Period

--- BudgetSystem/BudgetService.cs
+++ BudgetSystem/BudgetService.cs
@@ -8,33 +8,18 @@ public class Period
         End = end;
     }
 
-    public DateTime End { get; private set; }
-    public DateTime Start { get; private set; }
+    private DateTime End { get; set; }
+    private DateTime Start { get; set; }
 
     public int OverlappingDays(Budget budget)
     {
-        DateTime overlappingEnd = End < budget.LastDay()
+        var overlappingEnd = End < budget.LastDay()
             ? End
             : budget.LastDay();
 
-        DateTime overlappingStart = Start > budget.FirstDay()
+        var overlappingStart = Start > budget.FirstDay()
             ? Start
             : budget.FirstDay();
-        if (budget.YearMonth == Start.ToString("yyyyMM"))
-        {
-            // overlappingEnd = budget.LastDay();
-            // overlappingStart = Start;
-        }
-        else if (budget.YearMonth == End.ToString("yyyyMM"))
-        {
-            // overlappingEnd = End;
-            // overlappingStart = budget.FirstDay();
-        }
-        else
-        {
-            // overlappingEnd = budget.LastDay();
-            // overlappingStart = budget.FirstDay();
-        }
 
         return (overlappingEnd - overlappingStart).Days + 1;
     }
