2accfa59a342c76457a51a9a2e75096421d32a3a
Joey Chen <jay.hatelove@gmail.com>
2024-01-06 19:12:51 +0800

    inline method

--- BudgetSystem/BudgetService.cs
+++ BudgetSystem/BudgetService.cs
@@ -44,7 +44,54 @@ public class BudgetService
             data.Add(dailyAmountObject);
         }
 
-        var dayRange = GetMonthsWithDaysInRange(start, end);
+        List<DateResult>? dayRange;
+        List<DateResult> result = new List<DateResult>();
+
+        if (start.Year == end.Year && start.Month == end.Month)
+            dayRange = new List<DateResult>()
+                       {
+                           new DateResult()
+                           {
+                               Year = start.Year,
+                               Month = start.Month,
+                               Days = end.Day - start.Day + 1
+                           }
+                       };
+        else
+        {
+            var startDays = DateTime.DaysInMonth(start.Year, start.Month);
+
+            result.Add(new DateResult()
+                       {
+                           Year = start.Year,
+                           Month = start.Month,
+                           Days = startDays - start.Day + 1
+                       });
+
+            result.Add(new DateResult()
+                       {
+                           Year = end.Year,
+                           Month = end.Month,
+                           Days = end.Day
+                       });
+
+            var secondMonth = start.AddMonths(1);
+
+            for (var date = secondMonth; date < end; date.AddMonths(1))
+            {
+                if (date.Year == end.Year && date.Month == end.Month)
+                    break;
+
+                result.Add(new DateResult()
+                           {
+                               Year = date.Year,
+                               Month = date.Month,
+                               Days = DateTime.DaysInMonth(date.Year, date.Month)
+                           });
+            }
+
+            dayRange = result;
+        }
 
         return dayRange.Join(data,
                              day => new { day.Year, day.Month },
