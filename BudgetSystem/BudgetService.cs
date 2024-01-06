a1240942b6f54672a838ba20de4a26cffc4c7282
Joey Chen <jay.hatelove@gmail.com>
2024-01-06 19:28:57 +0800

    move end month calculation into for loop

--- BudgetSystem/BudgetService.cs
+++ BudgetSystem/BudgetService.cs
@@ -65,43 +65,53 @@ public class BudgetService
                 //                   });
                 // }
                 // else
-                if (budget.YearMonth == end.ToString("yyyyMM"))
+                // if (budget.YearMonth == end.ToString("yyyyMM"))
+                // {
+                //     dateRange.Add(new DateResult()
+                //                   {
+                //                       Year = end.Year,
+                //                       Month = end.Month,
+                //                       Days = end.Day
+                //                   });
+                // }
+                // else
+                // {
+                var nextMonthOfEnd = new DateTime(end.Year, end.Month, 1).AddMonths(1);
+                for (var currentMonth = start; currentMonth < nextMonthOfEnd; currentMonth.AddMonths(1))
                 {
-                    dateRange.Add(new DateResult()
-                                  {
-                                      Year = end.Year,
-                                      Month = end.Month,
-                                      Days = end.Day
-                                  });
-                }
-                else
-                {
-                    for (var currentMonth = start; currentMonth < end; currentMonth.AddMonths(1))
-                    {
-                        if (currentMonth.Year == end.Year && currentMonth.Month == end.Month)
-                            break;
+                    if (currentMonth.Year == end.Year && currentMonth.Month == end.Month)
+                        break;
 
-                        if (budget.YearMonth == start.ToString("yyyyMM"))
-                        {
-                            var startDays = DateTime.DaysInMonth(start.Year, start.Month);
-                            dateRange.Add(new DateResult()
-                                          {
-                                              Year = start.Year,
-                                              Month = start.Month,
-                                              Days = startDays - start.Day + 1
-                                          });
-                        }
-                        else
-                        {
-                            dateRange.Add(new DateResult()
-                                          {
-                                              Year = currentMonth.Year,
-                                              Month = currentMonth.Month,
-                                              Days = DateTime.DaysInMonth(currentMonth.Year, currentMonth.Month)
-                                          });
-                        }
+                    if (budget.YearMonth == start.ToString("yyyyMM"))
+                    {
+                        var startDays = DateTime.DaysInMonth(start.Year, start.Month);
+                        dateRange.Add(new DateResult()
+                                      {
+                                          Year = start.Year,
+                                          Month = start.Month,
+                                          Days = startDays - start.Day + 1
+                                      });
+                    }
+                    else if (budget.YearMonth == end.ToString("yyyyMM"))
+                    {
+                        dateRange.Add(new DateResult()
+                                      {
+                                          Year = end.Year,
+                                          Month = end.Month,
+                                          Days = end.Day
+                                      });
+                    }
+                    else
+                    {
+                        dateRange.Add(new DateResult()
+                                      {
+                                          Year = currentMonth.Year,
+                                          Month = currentMonth.Month,
+                                          Days = DateTime.DaysInMonth(currentMonth.Year, currentMonth.Month)
+                                      });
                     }
                 }
+                // }
             }
         }
 
